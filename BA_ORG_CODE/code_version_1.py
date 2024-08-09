3308472113aac9b7b9a46b2040ce1c39739f13c1
yalicia <ayocani@yahoo.com>
2024-08-09 12:25:42 +0200

    comment

--- BA_ORG_CODE/code_version_1.py
+++ BA_ORG_CODE/code_version_1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Excel-Datei einlesen
-file_path = '/Users/alicetangyie/Downloads/Uni/BachelorArbeit/GI_annuities_data_template.v10_GE_2024_Q1.xlsx'
+file_path = '/Users/alicetangyie/Downloads/Uni/BachelorArbeit/GI_annuities_data_template.v10_GE_2024_Q1.xlsx' # Muss von Anwender angepasst werden
 bestand_df = pd.read_excel(file_path, sheet_name="Inputs - MPs", header=5)
 sterbetafel_df = pd.read_excel(file_path, sheet_name="Death Table", header=3)
 
