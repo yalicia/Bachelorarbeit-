191c1a962394ee8a65c7c45ff21175a44b329c98
yalicia <ayocani@yahoo.com>
2024-08-09 12:18:18 +0200

    "Updated file path and sheet names for Excel file import"

--- BA_ORG_CODE/code_version_1.py
+++ BA_ORG_CODE/code_version_1.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 # Excel-Datei einlesen
-file_path = "pfad_zur_excel_datei.xlsx"
-bestand_df = pd.read_excel(file_path, sheet_name="Bestand")
-sterbetafel_df = pd.read_excel(file_path, sheet_name="Tabelle 2")
+file_path = '/Users/alicetangyie/Downloads/Uni/BachelorArbeit/GI_annuities_data_template.v10_GE_2024_Q1.xlsx'
+bestand_df = pd.read_excel(file_path, sheet_name="Inputs - MPs", header=5)
+sterbetafel_df = pd.read_excel(file_path, sheet_name="Death Table", header=3)
 
 # Überblick über die Spalten
 print(bestand_df.columns)
